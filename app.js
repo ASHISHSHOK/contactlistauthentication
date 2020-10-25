c41c108ad10d5cc5399820be4427e85cdc14e5f2
ASHISHSHOK <71622435+ASHISHSHOK@users.noreply.github.com>
2020-10-25 16:19:15 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
+//define the variables for the dependencies we need
+
 var express = require('express');
-var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var path = require('path');
-
-
-
-
 var app = express();
 
-/* Mongoose init and schema load */
+//declaring variable for mongoose
+var mongoose = require('mongoose');
+
+
+/* Mongoose conncection with app */
+//usercollection is our database name
 mongoose.connect("mongodb://localhost:27017/usercollection", {useNewUrlParser: true, useUnifiedTopology: true});
+
+//loading the configuration file for database
 var teacherModel = require('./models/config');
+
+//using the node modules
 app.use(bodyParser.json());
 app.use(express.static(__dirname+'/client'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -21,12 +27,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-
 app.set('views', path.join(__dirname, 'views'));
 
 
-/* routers */
+//routing the routs for our web application
 var router = express.Router();
 
 router.route('/contacts')
@@ -40,16 +44,17 @@ router.route('/contact/:id')
 
 app.use(router);
 
+//setting ejs as a template engine 
+app.set('view engine', 'ejs');
 app.engine('html',require('ejs').renderFile);
 
-app.set('view engine', 'ejs');
-
+//routing the ejs templates (html pages of the website)
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-/* App Configs */
+//port configuration
 var port = process.env.PORT || 80;
 app.listen(port);
 console.log('Express started at Port 80');
